013b5a777cf6d2751a6f3d0e5a552829085bcb65
Uten <mikael.1.kodehode@google.com>
2026-05-26 10:01:20 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,90 @@
+// Dropdown menu dice type
+
+const diceType = [`D4`, `D6`, `D8`, `D10`, `D12`, `D20`];
+const diceTypeSelect = document.getElementById(`dice-type`);
+
+diceType.forEach(function (type) {
+  const option = document.createElement(`option`);
+  option.value = type;
+  option.textContent = type;
+  diceTypeSelect.appendChild(option);
+});
+
+// Amount of dice to roll
+
+const diceSelect = document.getElementById(`dice-number`);
+
+for (let i = 1; i <= 20; i++) {
+  const option = document.createElement(`option`);
+  option.value = i;
+  option.textContent = i;
+  diceSelect.appendChild(option);
+}
+
+// Roll History
+const rollHistory = [];
+const rollHistoryList = document.getElementById(`dice-roll-history`);
+
+// The roll
+
+function rollDice() {
+  const diceTypeValue = diceTypeSelect.value.replace(/[a-zA-Z]/g, ``); // choosing dice type
+  const sides = parseInt(diceTypeValue); // extract number from dice
+  const amount = parseInt(diceSelect.value); // amount of dice rolled
+  const minimum = amount; // for the min/max display
+  const maximum = sides * amount; // for the min/max display
+
+  let total = 0;
+  let results = [];
+
+  for (let i = 0; i < amount; i++) {
+    const roll = Math.floor(Math.random() * sides) + 1;
+    total += roll;
+    results.push(roll);
+  }
+
+  // Build and push history entry
+  const historyEntry = `${amount} x D${sides} (${results.join(`, `)}) = ${total}`;
+  rollHistory.push(total);
+
+  // Create and append list item
+  const listItem = document.createElement("li");
+  listItem.textContent = historyEntry;
+  rollHistoryList.appendChild(listItem);
+
+  //   Display total
+  document.getElementById(`result-total`).textContent = `You rolled: ${total}`;
+
+  //   Display indivudual rolls
+  document.getElementById(`individual-rolls`).textContent =
+    `Individual results: ${results.join(`, `)}`;
+
+  // Min/Max Result
+  document.getElementById(`min-roll`).textContent = `Minimum roll: ${minimum}`;
+  document.getElementById(`max-roll`).textContent = `Maximum roll: ${maximum}`;
+}
+
+// Roll dice error
+document.getElementById(`dice-roller`).addEventListener("click", () => {
+  const amount = parseInt(diceSelect.value);
+
+  if (!amount || amount < 1) {
+    window.alert("Choose a dice type and amount of dice to roll!");
+    return;
+  }
+
+  rollDice();
+});
+
+// Clear Results
+function clearResults() {
+  document.getElementById(`result-total`).textContent = ``;
+  document.getElementById(`individual-rolls`).textContent = ``;
+  document.getElementById(`min-roll`).textContent = ``;
+  document.getElementById(`max-roll`).textContent = ``;
+}
+
+// Clear History
+function clearHistory() {
+  document.getElementById(`dice-roll-history`).textContent = ``;
+}
